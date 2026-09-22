06c60e5ef193f4c196087bdcec48bc71d991f97f
tonni-alter-dev <tanniakhter123@gmail.com>
2026-09-22 18:02:21 +0600

    jhj[A

--- app/redux/axios/axiosInstance.ts
+++ app/redux/axios/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
   timeout: 40000,
   headers: {
     "Content-Type": "application/json",
